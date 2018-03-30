494bf16641ee23959091424848f92309b4c677ac
PoisonArrowFrog <mikericcio@yahoo.com>
2018-03-30 12:10:42 -0700

    Filling out ChooseNextWaypoint Task Node C++ class.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,12 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "Classes/BehaviorTree/BehaviorTreeComponent.h"
+#include "Classes/BehaviorTree/BlackboardComponent.h"
 
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("C++ ChooseNextWaypoint"));
+	auto BlackboardCompoonent = OwnerComp.GetBlackboardComponent();
+	int32 Index = BlackboardCompoonent->GetValueAsInt(IndexKey.SelectedKeyName);
+
+	//UE_LOG(LogTemp, Warning, TEXT("C++ ChooseNextWaypoint"));
 
 	return EBTNodeResult::Succeeded;
 }
--- Source/TestingGrounds/ChooseNextWaypoint.h
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -15,4 +15,10 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 	GENERATED_BODY()
 
 	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;	
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+
+	struct FBlackboardKeySelector IndexKey;
 };
